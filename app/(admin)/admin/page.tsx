1db20a1f55a639a90997365390c66c90e1f568aa
xprofanity <pecpalmplay@gmail.com>
2025-11-22 09:37:20 +0200

    added clerk admin route protection

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,5 +1,14 @@
+import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import React from "react";
 
-export default function Admin() {
+export default async function Admin() {
+  const clerk = await clerkClient();
+  const user = await currentUser();
+  const authObject = await auth();
+
+  const role = authObject.orgRole;
+
+  console.log("role", role);
+
   return <div>Admin Page</div>;
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Home() {
+export default async function Home() {
   return <div>Home</div>;
 }
